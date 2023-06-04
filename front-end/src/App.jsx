fff5f4a9f95f9b6c39434acef5b7bfcad9266eb5
shannonjohn <shanjohncantz@example.com>
2023-06-05 02:24:43 +0800

    first commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import Sidebar from './components/sidebar'
+import './App.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Flex, Text, IconButton } from '@chakra-ui/react'
+import { FiMenu } from 'react-icons/fi'
+
+function App() {
+
+
+  return (
+    <ChakraProvider>
+    <Flex w="100%">
+      <Sidebar />
+      <Flex
+        pos="absolute"
+        top="50%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+      >
+        <Text>
+          <IconButton
+            background="none"
+            _hover={{ background: 'none' }}
+            icon={<FiMenu />}
+          />
+        </Text>
+      </Flex>
+    </Flex>
+    </ChakraProvider>
+  )
+}
+
+export default App
---
+++ front-end/src/components/Sidebar.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import {
+    Flex,
+    Text,
+    IconButton,
+    Divider,
+    Avatar,
+    Heading
+} from '@chakra-ui/react'
+import {
+    FiMenu,
+    FiHome,
+    FiCalendar,
+    FiUser,
+    FiPieChart
+} from 'react-icons/fi'
+
+import NavItem from '../components/NavItem'
+
+import React from 'react'
+
+const Sidebar = () => {
+    const [navSize, changeNavSize] = useState("large")
+    return (
+        <Flex
+            pos="sticky"
+            left="5"
+            h="95vh"
+            marginTop="2.5vh"
+            boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
+            borderRadius={navSize == "small" ? "15px" : "30px"}
+            w={navSize == "small" ? "75px" : "200px"}
+            flexDir="column"
+            justifyContent="space-between"
+        >
+            <Flex
+                p="5%"
+                flexDir="column"
+                w="100%"
+                alignItems={navSize == "small" ? "center" : "flex-start"}
+                as="nav"
+            >
+                <IconButton
+                    background="none"
+                    mt={5}
+                    _hover={{ background: 'none' }}
+                    icon={<FiMenu />}
+                    onClick={() => {
+                        if (navSize == "small")
+                            changeNavSize("large")
+                        else
+                            changeNavSize("small")
+                    }}
+                />
+                <NavItem navSize={navSize} icon={FiHome} title="Dashboard" description="This is the description for the dashboard." />
+                <NavItem navSize={navSize} icon={FiCalendar} title="Bookings" />
+                <NavItem navSize={navSize} icon={FiUser} title="Rooms" />
+                <NavItem navSize={navSize} icon={FiPieChart} title="Analytics" />
+                
+            </Flex>
+
+            <Flex
+                p="5%"
+                flexDir="column"
+                w="100%"
+                alignItems={navSize == "small" ? "center" : "flex-start"}
+                mb={4}
+            >
+                <Divider display={navSize == "small" ? "none" : "flex"} />
+                <Flex mt={4} align="center">
+                    <Avatar size="sm" src="avatar-1.jpg" />
+                    <Flex flexDir="column" ml={4} display={navSize == "small" ? "none" : "flex"}>
+                        <Heading as="h3" size="sm">Shannon John</Heading>
+                        <Text color="gray">Admin</Text>
+                    </Flex>
+                </Flex>
+            </Flex>
+        </Flex>
+    )
+}
+
+export default Sidebar
