90310538f2bf9b3d3889990138b775057771cb04
shannonjohn <shanjohncantz@example.com>
2023-06-07 00:58:52 +0800

    add components for each route

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 import Sidebar from './components/sidebar'
 import './App.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Flex, Text, IconButton } from '@chakra-ui/react'
 import { FiMenu } from 'react-icons/fi'
-import {} from 'react-router-dom'
+import {BrowserRouter,RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Analytics from './components/Analytics'
+import Dashboard from './components/Dashboard'
+import Rooms from './components/Rooms'
+import Bookings from './components/Bookings'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard/>
+  },
+  {
+    path: '/bookings',
+    element: <Bookings/>
+  },
+  {
+    path: '/rooms',
+    element: <Rooms/>
+  },
+  {
+    path: '/analytics',
+    element: <Analytics/>
+  }
+])
 
 function App() {
 
 
   return (
     <ChakraProvider>
-    <Flex w="100%">
-      <Sidebar />
-      <Flex
-        pos="absolute"
-        top="50%"
-        left="50%"
-        transform="translate(-50%, -50%)"
-      >
-        <Text>
-          <IconButton
-            background="none"
-            _hover={{ background: 'none' }}
-            icon={<FiMenu />}
-          />
-        </Text>
-      </Flex>
-    </Flex>
+      <RouterProvider router = {router}>
+        <Flex w="100%">
+          <Sidebar />
+          <Flex
+            pos="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+          >
+            <Text>
+              <IconButton
+                background="none"
+                _hover={{ background: 'none' }}
+                icon={<FiMenu />}
+              />
+            </Text>
+          </Flex>
+        </Flex>
+      </RouterProvider>
     </ChakraProvider>
   )
 }
