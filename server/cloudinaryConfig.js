c52ff2905b27b06cdc63284037fa8e4e3872ef20
shannonjohn <shanjohncantz@example.com>
2023-06-10 00:34:57 +0800

    setup cloudinary config

---
+++ server/cloudinaryConfig.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'dppn7c2efE',
+  api_key: '736163249684689',
+  api_secret: 'moCmIBKfuKpKQj8fBYhfs-FCZaE'
+});
+
+module.exports = cloudinary;
