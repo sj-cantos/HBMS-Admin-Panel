9748d914ce1e5ebd8f3cd202d92901f2031cd449
shannonjohn <shanjohncantz@example.com>
2023-06-10 00:45:14 +0800

    test upload sample image in cloudinary

--- server/cloudinaryConfig.js
+++ server/cloudinaryConfig.js
@@ -1,9 +1,18 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'dppn7c2efE',
+  cloud_name: 'dppn7c2ef',
   api_key: '736163249684689',
   api_secret: 'moCmIBKfuKpKQj8fBYhfs-FCZaE'
 });
+
+//test upload image in cloudinary server
+cloudinary.uploader.upload('./testpic.jpg', (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(result);
+    }
+  });
 
 module.exports = cloudinary;
