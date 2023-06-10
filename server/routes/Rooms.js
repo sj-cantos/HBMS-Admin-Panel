fc86796f34ae69f329a7ae91bdbeca2091f173a7
shannonjohn <shanjohncantz@example.com>
2023-06-11 00:53:05 +0800

    add get route for all rooms

--- server/routes/Rooms.js
+++ server/routes/Rooms.js
@@ -8,4 +8,17 @@ rooms.get('/test', checkAuth, (req, res) => {
   res.send('Hello admin ' + req.user.id);
 });
 
+rooms.get('/get', (req,res)=>{
+    pool.execute('SELECT * FROM `room_types`',(err,result)=>{
+      if(err){
+        res.send("Database error");
+      }else{
+        res.json(result);
+      }
+    })
+})
+
+
+
+
 module.exports = rooms;
