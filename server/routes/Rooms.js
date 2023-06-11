e20d0763fae0fc5f106c03c2542ad62945311ed6
shannonjohn <shanjohncantz@example.com>
2023-06-11 23:19:28 +0800

    Fixed get rooms route

--- server/routes/Rooms.js
+++ server/routes/Rooms.js
@@ -11,7 +11,10 @@ rooms.get('/test', checkAuth, (req, res) => {
 // Define the route handler for GET request
 rooms.get('/get', (req, res) => {
   pool.execute(`
-  SELECT * FROM room_types`,
+  SELECT room_types.*, roomimages.room_url
+  FROM room_types
+  JOIN roomimages ON room_types.id = roomimages.room_type
+`,
     (err, result) => {
       if (err) {
         res.status(500).send("Database error" + err.message);
@@ -19,17 +22,24 @@ rooms.get('/get', (req, res) => {
       
         
           
-          const response = result.map((row)=>({
-            "id" : row.id,
-            "room_type" : row.room_type,
-            "amenities" : row.amenities,
-            "status": row.status,
-            "price": row.price,
-            "bed_type": row.bed_type,
-            "pictures" : []
-          }))
-
-          res.send(response);
+        const rooms = {};
+        result.forEach((row) => {
+          const roomId = row.id;
+          if (!rooms[roomId]) {
+            rooms[roomId] = {
+              id: row.id,
+              name: row.room_type,
+              amenities: row.amenities,
+              status: row.status,
+              price: row.price,
+              bed_type: row.bed_type,
+              images: []
+            };
+          }
+          rooms[roomId].images.push(row.room_url);
+        });
+    
+        res.json(Object.values(rooms));
           
       }
     });
