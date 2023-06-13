a2f52f04bd60af20d32e43ef3d5f3a9fe34749f6
shannonjohn <shanjohncantz@gmail.com>
2023-06-14 01:53:31 +0800

    created theme token

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,14 +2,24 @@
 import './App.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from './Layout'
+import { extendTheme } from "@chakra-ui/react";
 
+const theme = extendTheme({
+  colors: {
+    primary: "#0E2954", // Your primary color
+    secondary: "#1F6E8C",
+    tertiary: "#2E8A99",
+    alternative: "#84A7A1"
+  },
+
+});
 
 
 function App() {
 
 
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Layout/>
     </ChakraProvider>
   )
