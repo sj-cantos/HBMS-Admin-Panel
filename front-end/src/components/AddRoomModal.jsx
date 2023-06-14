fa085e6b3a592c12ebde16f2b5917543c4286f85
shannonjohn <shanjohncantz@gmail.com>
2023-06-15 05:37:28 +0800

    minor fix

--- front-end/src/components/AddRoomModal.jsx
+++ front-end/src/components/AddRoomModal.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react';
 import { Image } from 'cloudinary-react';
-import { ImageUploader } from 'cloudinary-react';
 import axios from 'axios';
 
 const AddRoomModal = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [newRoomData, setNewRoomData] = useState({});
   const [selectedImage, setSelectedImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState('');
 
   const handleOpen = () => {
     setIsOpen(true);
@@ -35,12 +35,13 @@ const AddRoomModal = () => {
         }
       );
   
-      const publicId = response.data.public_id; 
-      const imageUrl = `https://res.cloudinary.com/dppn7c2ef/image/upload/${publicId}`;
+      const publicId = response.data.public_id;
+      const generatedUrl = `https://res.cloudinary.com/dppn7c2ef/image/upload/${publicId}`;
+  
       // Send the room data and image URL to the backend
       const roomData = {
         ...newRoomData,
-        imageUrl, // Add the image URL to the room data
+        imageUrl: generatedUrl, // Add the image URL to the room data
       };
   
       const backendResponse = await axios.post('http://localhost:3003/rooms/', {
@@ -56,6 +57,12 @@ const AddRoomModal = () => {
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     setSelectedImage(file);
+  
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImageUrl(reader.result);
+    };
+    reader.readAsDataURL(file);
   };
 
   return (
@@ -89,33 +96,20 @@ const AddRoomModal = () => {
                 <FormLabel>Amenities</FormLabel>
                 <Input
                   placeholder="Amenities"
-                  height="100px"
                   onChange={(e) => setNewRoomData({ ...newRoomData, amenities: e.target.value })}
                 />
-                <FormLabel>Images</FormLabel>
-                <ImageUploader
-                  cloudName="dppn7c2ef"
-                  uploadPreset="qvq5juo7" // Replace with your unsigned upload preset name
-                  onChange={handleImageUpload}
-                  buttonText="Upload Image"
-                />
-                {selectedImage && (
-                  <Image
-                    cloudName="dppn7c2ef"
-                    publicId={selectedImage.name}
-                    width="100"
-                    crop="scale"
-                  />
-                )}
+                <FormLabel>Image</FormLabel>
+                <Input type="file" accept="image/*" onChange={handleImageUpload} />
+                {imageUrl && <Image src={imageUrl} alt="Room" />}
               </Stack>
             </FormControl>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="teal" mr={3} onClick={handleSave}>
-              Save
-            </Button>
             <Button variant="ghost" onClick={handleClose}>
               Cancel
+            </Button>
+            <Button colorScheme="blue" mr={3} onClick={handleSave}>
+              Save
             </Button>
           </ModalFooter>
         </ModalContent>
