2ca83caa2c25c824b88a298207160c0f0142cd2d
shannonjohn <shanjohncantz@gmail.com>
2023-06-22 12:59:15 +0800

    acc icons to widgets

--- front-end/src/pages/Dashboard.jsx
+++ front-end/src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Stack, Text, Flex, Box } from '@chakra-ui/react';
+import { Stack, Text, Flex, Box,Icon } from '@chakra-ui/react';
 import { Card, CardHeader, CardBody, CardFooter ,Heading} from '@chakra-ui/react'
 import { BsBoxArrowInRight, BsBoxArrowInLeft } from 'react-icons/bs';
+import { AiFillBook } from "react-icons/ai";
+import { BsFillPeopleFill } from "react-icons/bs";
 
 const Dashboard = () => {
   // Example data for check-ins and check-outs
@@ -13,30 +15,45 @@ const Dashboard = () => {
       <Text color="teal.900" fontSize="35px" fontWeight="normal">
         Dashboard
       </Text>
-      <Flex mt={4} direction={['column', 'row']} gap="4">
-        <Box width="250px"  height="120px" bgColor="white" p={4} borderRadius="10" shadow="lg" mb={[4, 0]}>
-          <Heading size="md" fontWeight="semibold" color="gray.500"> Total Bookings</Heading>  
-          <Text mt="14px" fontWeight="semibold" fontSize="25px">20</Text>  
-        </Box>
-        <Box width="250px"  height="120px" bgColor="white" p={4} borderRadius="10" shadow="lg" mb={[4, 0]}>
-          <Heading size="md" fontWeight="semibold" color="gray.500"> Total Guests</Heading>  
-          <Text mt="14px" fontSize="25px" fontWeight="semibold">20</Text>  
-        </Box>
-      </Flex>
-      <Flex mt={4} direction={['column', 'row']} gap="4">
-        
-        <Box width="250px" height="120px" bgColor="white" p={4} borderRadius="10" shadow="lg">
-          <Flex alignItems="center">
-            <BsBoxArrowInRight/>
-            <Box ml = "30px">
-              <Heading size="md" fontWeight="semibold" fontSize="20px" color="gray.500"> Check-ins</Heading>  
-              <Text mt="14px" fontSize="25px" fontWeight="semibold">20</Text> 
+      <Flex mt={6} direction={['column', 'row']} gap="4">
+      <Box width="250px" height="100px" bgColor="white" p={4} borderRadius="10" shadow="lg">
+          <Flex alignItems="center">   
+            <Box ml = "10px" alignItems="center">
+              <Heading  fontWeight="semibold" fontSize="18px" color="gray.500"> Bookings</Heading>  
+              <Text mt="5px" fontSize="25px" fontWeight="semibold">20</Text> 
             </Box> 
+            <Icon as={AiFillBook} boxSize={10} color="teal"  p={1} borderRadius="10" ml="79px"/> 
           </Flex>
         </Box>
-        <Box width="250px"  height="120px" bgColor="white" p={4} borderRadius="10" shadow="lg">
-          <Heading size="md" fontWeight="semibold" color="gray.500"> Check-outs</Heading>  
-          <Text mt="14px" fontSize="25px" fontWeight="semibold">20</Text>  
+        <Box width="250px" height="100px" bgColor="white" p={4} borderRadius="10" shadow="lg">
+          <Flex alignItems="center">   
+            <Box ml = "10px" alignItems="center">
+              <Heading  fontWeight="semibold" fontSize="18px" color="gray.500">Guests</Heading>  
+              <Text mt="5px" fontSize="25px" fontWeight="semibold">20</Text> 
+            </Box> 
+            <Icon as={BsFillPeopleFill} boxSize={10} color="teal"  p={1} borderRadius="10" ml="100px"/> 
+          </Flex>
+        </Box>
+      </Flex>
+      <Flex mt={6} direction={['column', 'row']} gap="4">
+        
+        <Box width="250px" height="100px" bgColor="white" p={4} borderRadius="10" shadow="lg">
+          <Flex alignItems="center">   
+            <Box ml = "10px" alignItems="center">
+              <Heading  fontWeight="semibold" fontSize="18px" color="gray.500"> Check-ins</Heading>  
+              <Text mt="5px" fontSize="25px" fontWeight="semibold">20</Text> 
+            </Box> 
+            <Icon as={BsBoxArrowInRight} boxSize={10} color="teal"  p={1} borderRadius="10" ml="70px"/> 
+          </Flex>
+        </Box>
+        <Box width="250px" height="100px" bgColor="white" p={4} borderRadius="10" shadow="lg">
+          <Flex alignItems="center">   
+            <Box ml = "8px" alignItems="center">
+              <Heading  fontWeight="semibold" fontSize="18px" color="gray.500">Check-outs</Heading>  
+              <Text mt="5px" fontSize="25px" fontWeight="semibold">20</Text> 
+            </Box> 
+            <Icon as={BsBoxArrowInLeft} boxSize={10} color="teal"  p={1} borderRadius="10" ml="70px"/> 
+          </Flex>
         </Box>
       </Flex>
     </Box>
