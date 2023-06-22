bd2dd22ba918d7ab20db813f51d4c7479976bee8
shannonjohn <shanjohncantz@gmail.com>
2023-06-22 21:32:06 +0800

    add get route for widget data

--- server/routes/Dashboard.js
+++ server/routes/Dashboard.js
@@ -8,8 +8,62 @@ dashboard.get('/test', checkAuth, (req, res) => {
   res.send('Hello admin ' + req.user.id);
 });
 
-dashboard.get('/',(req,res)=>{
-  
-})
+dashboard.get('/widget', (req, res) => {
+  const today = new Date().toISOString().split('T')[0];
+
+  pool.execute(`SELECT COUNT(*) AS totalBookings FROM hotel_bookings`, (err, countRes) => {
+    if (err) {
+      res.status(500).json({ status: 500, message: "Server Error" });
+      console.log(err);
+    } else {
+      const totalBookings = countRes[0].totalBookings;
+
+      pool.execute(`SELECT SUM(num_guests) AS totalGuests FROM hotel_bookings`, (err, guestRes) => {
+        if (err) {
+          res.status(500).json({ status: 500, message: "Server Error" });
+          console.log(err);
+        } else {
+          const totalGuests = guestRes[0].totalGuests;
+
+          pool.execute(
+            `SELECT COUNT(*) AS checkIns FROM hotel_bookings WHERE check_in_date = ?`,
+            [today],
+            (err, checkInRes) => {
+              if (err) {
+                res.status(500).json({ status: 500, message: "Server Error" });
+                console.log(err);
+              } else {
+                const checkIns = checkInRes[0].checkIns;
+
+                pool.execute(
+                  `SELECT COUNT(*) AS checkOuts FROM hotel_bookings WHERE check_out_date = ?`,
+                  [today],
+                  (err, checkOutRes) => {
+                    if (err) {
+                      res.status(500).json({ status: 500, message: "Server Error" });
+                      console.log(err);
+                    } else {
+                      const checkOuts = checkOutRes[0].checkOuts;
+
+                      const response = {
+                        totalBookings,
+                        totalGuests,
+                        checkIns,
+                        checkOuts
+                      };
+
+                      res.json(response);
+                    }
+                  }
+                );
+              }
+            }
+          );
+        }
+      });
+    }
+  });
+});
+
 
 module.exports = dashboard;
