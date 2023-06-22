5bd626efb5525e15b04cd216c9cac97e41e0a7bf
shannonjohn <shanjohncantz@gmail.com>
2023-06-22 22:15:53 +0800

    fetched get endpoint for widget data

--- front-end/src/pages/Dashboard.jsx
+++ front-end/src/pages/Dashboard.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Stack, Text, Flex, Box,Icon } from '@chakra-ui/react';
 import { Card, CardHeader, CardBody, CardFooter ,Heading} from '@chakra-ui/react'
 import { BsBoxArrowInRight, BsBoxArrowInLeft } from 'react-icons/bs';
 import { AiFillBook } from "react-icons/ai";
 import { BsFillPeopleFill } from "react-icons/bs";
+import axios from 'axios'
+import { useState } from 'react';
 
 const Dashboard = () => {
   // Example data for check-ins and check-outs
-  const checkInCount = 10;
-  const checkOutCount = 5;
+  const [widgetData, setWidgetData] = useState({});
+
+  useEffect(()=>{
+    const getWidgetData = ()=>{
+      axios.get('http://localhost:3003/dashboard/widget')
+      .then(response => setWidgetData(response.data))
+      .catch((error) => console.log(error))
+    }
+    getWidgetData();
+  
+  }, [])
 
   return (
     <Box>
@@ -20,7 +31,7 @@ const Dashboard = () => {
           <Flex alignItems="center">   
             <Box ml = "10px" alignItems="center">
               <Heading  fontWeight="semibold" fontSize="18px" color="gray.500"> Bookings</Heading>  
-              <Text mt="5px" fontSize="25px" fontWeight="semibold">20</Text> 
+              <Text mt="5px" fontSize="25px" fontWeight="semibold">{widgetData.totalBookings}</Text> 
             </Box> 
             <Icon as={AiFillBook} boxSize={10} color="teal"  p={1} borderRadius="10" ml="79px"/> 
           </Flex>
@@ -29,7 +40,7 @@ const Dashboard = () => {
           <Flex alignItems="center">   
             <Box ml = "10px" alignItems="center">
               <Heading  fontWeight="semibold" fontSize="18px" color="gray.500">Guests</Heading>  
-              <Text mt="5px" fontSize="25px" fontWeight="semibold">20</Text> 
+              <Text mt="5px" fontSize="25px" fontWeight="semibold">{widgetData.totalGuests}</Text> 
             </Box> 
             <Icon as={BsFillPeopleFill} boxSize={10} color="teal"  p={1} borderRadius="10" ml="100px"/> 
           </Flex>
@@ -41,7 +52,7 @@ const Dashboard = () => {
           <Flex alignItems="center">   
             <Box ml = "10px" alignItems="center">
               <Heading  fontWeight="semibold" fontSize="18px" color="gray.500"> Check-ins</Heading>  
-              <Text mt="5px" fontSize="25px" fontWeight="semibold">20</Text> 
+              <Text mt="5px" fontSize="25px" fontWeight="semibold">{widgetData.checkIns}</Text> 
             </Box> 
             <Icon as={BsBoxArrowInRight} boxSize={10} color="teal"  p={1} borderRadius="10" ml="70px"/> 
           </Flex>
@@ -50,7 +61,7 @@ const Dashboard = () => {
           <Flex alignItems="center">   
             <Box ml = "8px" alignItems="center">
               <Heading  fontWeight="semibold" fontSize="18px" color="gray.500">Check-outs</Heading>  
-              <Text mt="5px" fontSize="25px" fontWeight="semibold">20</Text> 
+              <Text mt="5px" fontSize="25px" fontWeight="semibold">{widgetData.checkOuts}</Text> 
             </Box> 
             <Icon as={BsBoxArrowInLeft} boxSize={10} color="teal"  p={1} borderRadius="10" ml="70px"/> 
           </Flex>
