fd9820ed6460ab343ab5be7a3bfef5e347793724
shannonjohn <shanjohncantz@gmail.com>
2023-06-29 01:44:08 +0800

    make get route for weekly revenue

--- server/routes/Authenticate.js
+++ server/routes/Authenticate.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const authenticate = express.Router();
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const crypto = require('crypto');
-const pool = require('../database');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const crypto = require("crypto");
+const pool = require("../database");
 
-const checkAuth = require('./CheckAuth');
+const checkAuth = require("./CheckAuth");
 
-// ----------------------- login route ----------------------- 
+// ----------------------- login route -----------------------
 
 // localhost:PORT/login - temporary disabled in development.
 // authenticate.get('/', (req, res) => {
@@ -15,55 +15,78 @@ const checkAuth = require('./CheckAuth');
 //   res.sendFile('index.html', { root: './public'});
 // });
 
-authenticate.get('/check', checkAuth, (req, res) => {
-  res.status(200).json({user: req.user.id});
+authenticate.get("/check", checkAuth, (req, res) => {
+  res.status(200).json({ user: req.user.id });
 });
 
 // localhost:PORT/login
-authenticate.post('/', passport.authenticate('local'), function(req, res) {
+authenticate.post("/", passport.authenticate("local"), function (req, res) {
   res.json({ msg: "Login Success", code: 200 });
 });
 
 // localhost:PORT/login/signout
-authenticate.delete('/signout', checkAuth, (req, res, next) => {
+authenticate.delete("/signout", checkAuth, (req, res, next) => {
   req.logout((err) => {
     if (err) {
       return next(err);
     } else {
-      res.status(200).json({ msg: 'Successfully logged out', code: 200 });
+      res.status(200).json({ msg: "Successfully logged out", code: 200 });
     }
   });
 });
 
-// ----------------------- passport-local setup ----------------------- 
-passport.use(new LocalStrategy(function verify(username, password, callback) {
-  pool.execute('SELECT * FROM `admin_users` WHERE id = ?', [username], (err, result) => {
-    if (err) {
-      return callback(err); 
-    } else if (result.length === 0) {
-      return callback(null, false, { message: 'That user admin does not exists in the database'}); // for dev only
-      // return callback(null, false, { message: 'Incorrect username or password'}); // for release
-    } else if (result.length > 1) {
-      return callback(null, false, { message: 'That was not supposed to happen, the database table might be broken'}); // for dev only
-    }
+// ----------------------- passport-local setup -----------------------
+passport.use(
+  new LocalStrategy(function verify(username, password, callback) {
+    pool.execute(
+      "SELECT * FROM `admin_users` WHERE id = ?",
+      [username],
+      (err, result) => {
+        if (err) {
+          return callback(err);
+        } else if (result.length === 0) {
+          return callback(null, false, {
+            message: "That user admin does not exists in the database",
+          }); // for dev only
+          // return callback(null, false, { message: 'Incorrect username or password'}); // for release
+        } else if (result.length > 1) {
+          return callback(null, false, {
+            message:
+              "That was not supposed to happen, the database table might be broken",
+          }); // for dev only
+        }
 
-    crypto.pbkdf2(Buffer.from(password, 'utf-8').toString(), Buffer.from(result[0].salt, 'base64'), 200000, 64, 'sha512', function(err, hashedPassword) {
-      if (err) {
-        return callback(err);
-      } else if (!crypto.timingSafeEqual(hashedPassword, Buffer.from(result[0].hash, 'base64'))) {
-        return callback(null, false, { message: 'Incorrect Password'}); // for dev only
-        // return callback(null, false, { message: 'Incorrect username or password'}); // for release
+        crypto.pbkdf2(
+          Buffer.from(password, "utf-8").toString(),
+          Buffer.from(result[0].salt, "base64"),
+          200000,
+          64,
+          "sha512",
+          function (err, hashedPassword) {
+            if (err) {
+              return callback(err);
+            } else if (
+              !crypto.timingSafeEqual(
+                hashedPassword,
+                Buffer.from(result[0].hash, "base64")
+              )
+            ) {
+              return callback(null, false, { message: "Incorrect Password" }); // for dev only
+              // return callback(null, false, { message: 'Incorrect username or password'}); // for release
+            }
+
+            // this is where `passport.serializeUser()`s callback parameter `user` data comes from.
+            return callback(null, result[0]);
+          }
+        );
       }
-
-      // this is where `passport.serializeUser()`s callback parameter `user` data comes from.
-      return callback(null, result[0]);
-    });
-  });
-}));
+    );
+  })
+);
 
 // happens during username and password authentication (login).
-passport.serializeUser(function(user, callback) {
-  process.nextTick(function() {
+passport.serializeUser(function (user, callback) {
+  process.nextTick(function () {
     // stores the new structure of the `user` data to the session table.
     return callback(null, { id: user.id });
   });
@@ -71,13 +94,13 @@ passport.serializeUser(function(user, callback) {
 
 // happens everytime we `checkAuth` for an http route.
 // the `user` parameter is the extracted `user` data in the session table.
-passport.deserializeUser(function(user, callback) {
-  process.nextTick(function() {
+passport.deserializeUser(function (user, callback) {
+  process.nextTick(function () {
     // sets the `req.user` object on https requests after the `checkAuth` middleware.
     return callback(null, user);
   });
 });
 
-// ---------------------------------------------- 
+// ----------------------------------------------
 
 module.exports = authenticate;
--- server/routes/Reports.js
+++ server/routes/Reports.js
@@ -8,4 +8,24 @@ reports.get("/test", checkAuth, (req, res) => {
   res.send("Hello admin " + req.user.id);
 });
 
+reports.get('/weekly-revenue', (req, res) => {
+  const query = `
+    SELECT DATE_FORMAT(check_in_date, '%Y-%m') AS month,
+           DATE_SUB(check_in_date, INTERVAL (WEEKDAY(check_in_date) + 1) DAY) AS week_start_date,
+           SUM(amount_paid) AS revenue
+    FROM hotel_bookings
+    GROUP BY DATE_FORMAT(check_in_date, '%Y-%m'), DATE_SUB(check_in_date, INTERVAL (WEEKDAY(check_in_date) + 1) DAY)
+    ORDER BY month, week_start_date;
+  `;
+
+  pool.query(query, (error, results) => {
+    if (error) {
+      console.error('Error fetching weekly revenue:', error);
+      res.status(500).json({ error: 'Error fetching weekly revenue' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
 module.exports = reports;
