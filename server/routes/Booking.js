f2806aa350d2e82c382b1718b86ae6574e44834f
shannonjohn <shanjohncantz@gmail.com>
2023-07-10 21:42:36 +0800

    la lang

--- server/routes/Booking.js
+++ server/routes/Booking.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const booking = express.Router();
 const pool = require("../database");
+const checkAuth = require("./CheckAuth")
 
-const checkAuth = require("./CheckAuth");
 
-booking.get("/test", checkAuth, (req, res) => {
+booking.get("/test", checkAuth,  (req, res) => {
   res.send("Hello admin " + req.user.id);
 });
 
-booking.get("/", (req, res) => {
+booking.get("/",   (req, res) => {
   pool.query(
     `SELECT hotel_bookings.*, status.status_name,room_types.room_type
               FROM hotel_bookings
